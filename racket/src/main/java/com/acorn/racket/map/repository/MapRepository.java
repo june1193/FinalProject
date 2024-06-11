d1c21da7fde7da7548555c50e07245a28a2c276c
kirakiraBeom <qjawns3170@gmail.com>
2024-06-11 14:45:44 +0900

    메인페이지 DB연동, 페이지 이동 등 수정

--- racket/src/main/java/com/acorn/racket/map/repository/MapRepository.java
+++ racket/src/main/java/com/acorn/racket/map/repository/MapRepository.java
@@ -1,5 +1,30 @@
 package com.acorn.racket.map.repository;
 
-public class MapRepository {
+import java.util.List;
 
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.acorn.racket.map.domain.MapDTO;
+
+@Component
+public class MapRepository implements MapRepositoryI {
+    @Autowired
+    private DataSource ds;
+    
+    @Autowired
+    private SqlSession session;
+    
+    private static final String NAMESPACE = "test.MainMapper.";
+
+    public List<MapDTO> getUniqueFacilities() {
+        return session.selectList(NAMESPACE + "getUniqueFacilities");
+    }
+    
+    public List<MapDTO> getRecommendedPosts() {
+        return session.selectList(NAMESPACE + "getRecommendedPosts");
+    }
 }
