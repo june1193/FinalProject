c210e9a8bc9b20f852cf7f4abe6dd49265d4b8f8
jgw <cjsgh967@gmail.com>
2024-06-13 16:47:48 +0900

    정기원06-13

--- racket/src/main/java/com/acorn/racket/login/controller/LoginController.java
+++ racket/src/main/java/com/acorn/racket/login/controller/LoginController.java
@@ -10,24 +10,24 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class LoginController {
 	
-	@RequestMapping( value="/login2" , method=RequestMethod.GET)
-	public String login() {		
-		return "login";		
-	}	
-	
-	
-	@RequestMapping( value="/login" , method=RequestMethod.GET)
-	public String login2(String id, String pw , HttpServletRequest request  ) {	
-		//dao확인 
-		if(id.equals("test3") && pw.equals("0915")) {
-			//세션에 등록 
-		 	HttpSession  session  =request.getSession();
-		 	session.setAttribute("user", id);		
-			return "map";	
-		}
-		return "redirect:/map";
-			
-	}
-	
-	 
+//	@RequestMapping( value="/login2" , method=RequestMethod.GET)
+//	public String login() {		
+//		return "login";		
+//	}	
+//	
+//	
+//	@RequestMapping( value="/login" , method=RequestMethod.GET)
+//	public String login2(String id, String pw , HttpServletRequest request  ) {	
+//		//dao확인 
+//		if(id.equals("test3") && pw.equals("0915")) {
+//			//세션에 등록 
+//		 	HttpSession  session  =request.getSession();
+//		 	session.setAttribute("user", id);		
+//			return "map";	
+//		}
+//		return "redirect:/map";
+//			
+//	}
+//	
+//	 
 }
